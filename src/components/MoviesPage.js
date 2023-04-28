91ee887dc120ef38abe81521751d19e01ea67a4e
Serah-Njenga <serah35.nn@gmail.com>
2023-04-28 08:41:04 +0300

    commit

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+
 
 function MoviesPage({ movies }) {
   return (
     <div>
-      <MoviesList movies={movies} />
+      <MoviesPage movies={movies} />
     </div>
   );
 }
